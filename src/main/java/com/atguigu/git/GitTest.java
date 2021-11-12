c2b7902efebeab22b37e005d0046c9a674c77fbb
admin <admin@qq.com>
2021-11-12 15:37:48 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * @ClassName GitTest
+ * @Description TODO
+ * @Author my
+ * @Date 2021/11/12 15:30
+ * @Version 1.0
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
